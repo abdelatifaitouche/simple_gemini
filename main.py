67ee1241298a4bf75b5c5107fa6bc84113e9ff53
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-03-16 09:35:39 +0100

    wtf3

--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from chatbot import Chatbot
 
 app = FastAPI()
 
 chat = Chatbot("AIzaSyD9tBP0NaJ6s-vA_qEOwbBtAMu34g1tw9c")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
