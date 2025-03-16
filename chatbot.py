7fd10fa5a5af04d2f4c97b50cd9804b85d84c987
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-03-16 07:59:12 +0100

    wtf2

--- chatbot.py
+++ chatbot.py
@@ -8,6 +8,7 @@ class Chatbot :
     def __init__(self, API_KEY : str):
         self.API_KEY = API_KEY
         self.MODEL = "gemini-1.5-flash"
+        genai.configure(api_key=API_KEY)
         self.model = genai.GenerativeModel(self.MODEL)
 
     def generate(self , query : str):
